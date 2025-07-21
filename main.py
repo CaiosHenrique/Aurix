99d8e346741719a4262eed427750d6ed365cf215
CaiosHenrique <caiokaikai@gmail.com>
2025-07-21 17:56:57 -0300

    Refactor bot token handling and add .gitignore file

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+
 import discord
 from discord.ext import commands
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
 
 intents = discord.Intents.default()
-intents.message_content = True 
+intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
@@ -12,27 +18,12 @@ products = {}
 async def on_ready():
     print(f'Bot conectado como {bot.user}')
 
-@bot.event
-async def on_message(message):
-    print(f'Mensagem recebida: {message.content}')
-
 @bot.command()
-async def monitor(ctx, link: str):
-    products[link] = {'status': 'monitoring'}
-    await ctx.send(f'Produto {link} está sendo monitorado.')
-
-@bot.command()
-async def products(ctx):
-    if not products:
-        await ctx.send('Nenhum produto monitorado.')
-    else:
-        response = 'Produtos monitorados:\n'
-        for link, info in products.items():
-            response += f'{link}: {info["status"]}\n'
-        await ctx.send(response)
+async def test(ctx, arg):
+    await ctx.send(arg)
 
 @bot.command()
 async def ping(ctx):
     await ctx.send('Pong!')
 
-bot.run('MTM5Njk0NDY0OTIxNzc3MzYxOQ.GFy2dD.orRWhqFQzi9sxTsnbIpU-u2TpoxXVhesUwUn5s')
+bot.run(TOKEN)
